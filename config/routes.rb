c3faf8cd9b90b4b0f20e21590043e40c2f0ea2d3
GioLim <gionnieve@hotmail.com>
2019-01-04 11:33:28 +0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root 'login#index'
+  get 'welcome/index'
+  get 'app/views/login/index.html.erb', to: 'login#index', as: 'login'
+  get 'app/views/welcome/index.html.erb', to: 'welcome#index', as: 'welcome'
+  get 'app/views/advancedsearch/index.html.erb', to: 'advancedsearch#index', as: 'advancedsearch'
+
+  resources :login
+  resources :advancedsearch
+
+  root 'login#index'
 end
