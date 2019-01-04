6e5cb9ceb2264a7775a698b140126361bdfc2878
Ubuntu <ubuntu@ip-172-31-27-63.us-east-2.compute.internal>
2019-01-04 03:48:57 +0000

    integrated frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   get 'app/views/login/index.html.erb', to: 'login#index', as: 'login'
-  get 'app/views/welcome/index.html.erb', to: 'welcome#index', as: 'welcome'
+  get 'app/views/home/index.html.erb', to: 'home#index', as: 'home'
   get 'app/views/advancedsearch/index.html.erb', to: 'advancedsearch#index', as: 'advancedsearch'
 
+  resources :home
   resources :login
   resources :advancedsearch
 
