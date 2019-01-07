e4f9412bd07e340daf8edf4dfaf102f13fc316f7
avoneiff <avoneiff@tamu.edu>
2019-01-07 07:42:12 +0000

    added in stuff to get bookings table up and running, has 1 entry in it rn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,12 @@ class SessionsController < ApplicationController
   def create
      user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user
     else
       # Create an error message.
-    render 'new'
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
     end
   end
   
