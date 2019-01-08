3334c33cf435ac09a4545547acec2a05d77ccf65
lyqht <estee.tsw@gmail.com>
2019-01-08 02:15:46 +0000

    show my reservations on home page if no reservations

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,29 @@
 Rails.application.routes.draw do
   
 # For admin page
-    root 'bookings#index'
-    resources :users, :bookings, :rooms
+    # root 'bookings#index'
+    # resources :users, :bookings, :rooms
   
 
 # For styled pages
-  # root 'login#index'
+  root 'login#index'
   
-  # get 'sessions/new'
-  # get 'app/views/login/index.html.erb', to: 'login#index', as: 'login'
-  # get 'app/views/home/index.html.erb', to: 'home#index', as: 'home'
-  # get 'app/views/advancedsearch/index.html.erb', to: 'advancedsearch#index', as: 'advancedsearch'
-  # get 'app/views/browsingsearch/index.html.erb', to: 'browsingsearch#index', as: 'browsingsearch'
-  
-  # get    '/bookings',   to: '#bookings', as: 'bookings'
+  get 'sessions/new'
+  get 'app/views/login/index.html.erb', to: 'login#index', as: 'login'
+  get 'app/views/home/index.html.erb', to: 'home#index', as: 'home'
+  get 'app/views/advancedsearch/index.html.erb', to: 'advancedsearch#index', as: 'advancedsearch'
+  get 'app/views/browsingsearch/index.html.erb', to: 'browsingsearch#index', as: 'browsingsearch'
+  get 'app/views/rooms/index.html.erb', to: 'rooms#index', as: 'rooms'
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  # resources :home
-  # resources :login
-  # resources :advancedsearch
-  # resources :browsingsearch
-  # resources :user
-  # resources :sessions
+  resources :home
+  resources :login
+  resources :advancedsearch
+  resources :browsingsearch
+  resources :user
+  resources :sessions
+  resources :rooms, :bookings
 end
