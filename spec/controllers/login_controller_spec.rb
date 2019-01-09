6f33f97ff8e31126d176d78dbd966f21c6311b1c
EC2 Default User <ec2-user@ip-172-31-36-126.ap-southeast-1.compute.internal>
2019-01-09 02:25:26 +0000

    Added login tests

--- spec/controllers/login_controller_spec.rb
+++ spec/controllers/login_controller_spec.rb
@@ -2,6 +2,27 @@ require 'rails_helper'
 
 RSpec.describe LoginController, type: :controller do
   
-
-
+    before(:each) do
+        @user = User.new(name: "Example User", email: "user@example.com", password: "1234")
+    end
+    
+    it "name should be present" do
+    @user.name = ""
+    expect(@user).to_not be_valid
+    end
+    
+    it "email should be present" do
+    @user.email = "     "
+    expect(@user).to_not be_valid
+    end
+    
+    it "is not valid without an email" do
+      @user = build(:user, email: nil)
+      expect(@user).to_not be_valid
+    end
+    
+     it "is not valid without a password" do 
+      @user = build(:user, password: nil)
+      expect(@user).to_not be_valid
+    end
 end
