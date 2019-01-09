e39863b085417a8ff7e4fc31572dfe8d8f1f79ee
EC2 Default User <ec2-user@ip-172-31-8-240.us-east-2.compute.internal>
2019-01-09 03:34:04 +0000

    Simple Cov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,10 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
-
+require 'simplecov'
+#require 'simplecov-console'
+#SimpleCov.formatter = SimpleCov::Formatter::Console
+SimpleCov.start
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
