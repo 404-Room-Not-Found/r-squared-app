cb1b247a291861dcce7398c7f890270d2e6fe226
EC2 Default User <ec2-user@ip-172-31-36-126.ap-southeast-1.compute.internal>
2019-01-10 02:57:58 +0000

    browsing search test

--- spec/controllers/browsingsearch_controller_spec.rb
+++ spec/controllers/browsingsearch_controller_spec.rb
@@ -5,18 +5,18 @@ RSpec.describe BrowsingsearchController, type: :controller do
         visit browsingsearch_path
     end
     
-    it "redirects to RDMC-specific Rooms" do
-        click_link 'RDMC'
-        expect(page.current_path).to eq rdmc_path
-    end
-    
     it "redirects to HRBB-specific rooms" do
         click_link 'HRBB'
         expect(page.current_path).to eq bright_path
     end
     
-    it "redirects to All-Rooms" do
-        click_link 'All Rooms'
-        expect(page.current_path).to eq rooms_path
-    end
+    # it "redirects to RDMC-specific Rooms" do
+    #     click_link 'RDMC'
+    #     expect(page.current_path).to eq rdmc_path
+    # end
+    
+    # it "redirects to All-Rooms" do
+    #     click_link 'All Rooms'
+    #     expect(page.current_path).to eq rooms_path
+    # end
 end
