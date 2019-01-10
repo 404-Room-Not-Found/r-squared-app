f9fcf7b85f19745d9362d1a63cd022839b826ce9
avoneiff <avoneiff@tamu.edu>
2019-01-10 02:13:49 +0000

    preventing url bashing

--- app/controllers/browsingsearch_controller.rb
+++ app/controllers/browsingsearch_controller.rb
@@ -1,2 +1,9 @@
 class BrowsingsearchController < ApplicationController
+before_action :validate_access 
+private 
+  def validate_access 
+      unless logged_in?
+        redirect_to root_path
+      end 
+  end
 end
