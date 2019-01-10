25ba4b3ce0eeb93a779339c3c48a62b913da83f7
Tey Siew Wen <35736525+lyqht@users.noreply.github.com>
2019-01-10 18:54:09 +0800

    advancedsearch (#26)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,7 @@
 class BookingsController < ApplicationController
 before_action :validate_access 
   def booking_params
-    params.require(:booking).permit(:room_id,:booker_id,:reason,
+    params.require(:booking).permit(:room_id,:booker_id,
     :time_start,:time_end, :building_name)
   end
 
@@ -17,29 +17,10 @@ before_action :validate_access
 
   def new
     @booking = Booking.new
-    # attr_accessor :room_id, :booker_id, :reason, :time_start, :time_end
-    # def initialize(attributes = {})
-    #   @building_name = attributes[:building_name]
-    #   @room_id  = attributes[:room_id]
-    #   @booker_id = attributes[:booker_id]
-    #   @reason = attributes[:reason]
-    #   @time_start = attributes[:time_start]
-    #   @time_end = attributes[:time_end]
-    # end
   end
 
   def create
-  #   @building_name = params[:building_name]
-  #   @room_id  = params[:room_id]
-  #   @booker_id = User.where(:name => params[:id][:booker_name]).first.id
-  #   @reason = params[:reason]
-  #   @time_start = params[:time_start]
-  #   @time_end = params[:time_end]
-    
-    # @booking = Booking.create!(:building_name => @building_name, :room_id => @room_id, :booker_id => @booker_id, :reason => @reason,
-    # :time_start => @time_start, :time_end => @time_end)
-    
-    @booking = Booking.create!(booking_params) 
+    @booking = Booking.create!(booking_params)
     flash[:notice] = "#{@booking.id} was successfully created."
     redirect_to home_index_path
   end
