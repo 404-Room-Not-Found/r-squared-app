843a58c9b4d35814e408d3695562852f3e6bcca9
avoneiff <avoneiff@tamu.edu>
2019-01-10 01:44:50 +0000

    added in new booking rspec, commented out simplecov in gem

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,7 @@
 class Booking < ActiveRecord::Base
+    validates :building_name, presence: true
+    validates :room_id, presence: true
+    validates :booker_id, presence: true
+    validates :time_start, presence: true
+    validates :time_end, presence: true
 end
---
+++ spec/models/booking_spec.rb
@@ -0,0 +1,52 @@
+require "rails_helper"
+RSpec.describe Booking, :type => :model do
+  
+  before(:all) do
+    @test_Booking = Booking.new(
+     :building_name => 'HRBB', :room_id => 2, :booker_id => 2, 
+            :booker_name => 'Estee',
+            :reason => "Estee just wanna sleep ok, nothing new or fancy", 
+            :time_start=> DateTime.parse('3rd Feb 2001 04:05:06 PM'),
+            :time_end=>DateTime.parse('5th Feb 2001 04:05:06 PM'))
+  end
+  
+  it "is valid with all attributes filled up" do
+    expect(@test_Booking).to be_valid
+  end
+  
+  it "is invalid with missing info (building name)" do
+    @test_Booking.building_name = nil
+    expect(@test_Booking).to_not be_valid
+  end
+  
+  it "is invalid with missing info (room id)" do
+    @test_Booking.building_name = 'HRBB'
+    @test_Booking.room_id = nil
+    expect(@test_Booking).to_not be_valid
+  end
+  
+  it "is invalid with missing info (booker id)" do
+    @test_Booking.room_id = 2
+    @test_Booking.booker_id = nil
+    expect(@test_Booking).to_not be_valid
+  end
+  
+  it "is invalid with missing info (time start)" do
+    @test_Booking.booker_id = 2
+    @test_Booking.time_start = nil
+    expect(@test_Booking).to_not be_valid
+  end
+  
+  it "is invalid with missing info (time begin)" do
+    @test_Booking.time_start = DateTime.parse('3rd Feb 2001 04:05:06 PM')
+    @test_Booking.time_end = nil
+    expect(@test_Booking).to_not be_valid
+  end
+  
+  it "is valid with missing reason" do
+      @test_Booking.time_end = DateTime.parse('5th Feb 2001 04:05:06 PM')
+      @test_Booking.reason = nil
+      expect(@test_Booking).to be_valid
+    end
+  
+end  
