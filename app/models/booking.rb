678acc733c3bd554fc4173e16f44b3cc8d54bd8f
EC2 Default User <ec2-user@ip-172-31-36-126.ap-southeast-1.compute.internal>
2019-01-20 20:43:23 +0000

    Testing Overlap

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,13 @@ class Booking < ActiveRecord::Base
     validates :booker_id, presence: true
     validates :time_start, presence: true
     validates :time_end, presence: true
+    validate :time_overlaps
+    
+    def time_overlaps(other)
+        time_start <= other.time_end && other.time_start <= time_end
+    end
+    
+    # scope :overlap, -> { |booking|
+    #     where("id <> ? AND time_start <= ? AND ? <= end_date", booking.room_id, booking.time_end, booking.time_start)
+    # }
 end
